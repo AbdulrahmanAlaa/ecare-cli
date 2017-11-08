6d061869f577d6d1bec6a59952837c02b0edd8b7
DESKTOP-7CI91VE\Abdulrahman Alaa <abdelraman.bashir@vodafone.com>
2017-11-08 12:48:33 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const argv = require('yargs').argv
+const fs = require('fs');
+const handlebars = require('handlebars');
+const { join } = require('path');
+
+
+const data = {
+    mycompany: {
+        name: 'GASCO',
+    },
+
+};
+
+
+// Read source template
+const source = fs.readFileSync(join(`${__dirname}/template.js`), 'utf-8');
+
+// Convert to Handlebars template and add the data
+const template = handlebars.compile(source);
+const html = template(data);
+
+console.log(html);
+//GetCommand(argv.module);
+
+function GetCommand(command) {
+
+    var path = './modules/' + command + '/';
+    fs.mkdirSync(path);
+    fs.writeFileSync(path + command + '.module.js');
+    fs.writeFileSync(path + command + '.controller.js');
+    fs.writeFileSync(path + command + '.route.js');
+    fs.writeFileSync(path + command + '.service.js');
+    fs.writeFileSync(path + command + '.data.js');
+}
---
+++ template.js
@@ -0,0 +1,11 @@
+
+(function () {
+    "use strict";
+    {{company.name}}
+}());
+
+
+
+
+
+
